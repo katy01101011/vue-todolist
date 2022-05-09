12812d2db2830cd1f4fecc9c0d2a5b7669f0facc
katy01101011 <katy.91560@gmail.com>
2022-05-09 17:18:08 +0200

    Milestone1 :D

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-alert('Ciao')
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            todoList: [
+                {
+                    text: 'Fare l\'esercizio del pomeriggio',
+                    done: false
+                },
+                {
+                    text: 'Controllare le mail',
+                    done: false
+                },
+                {
+                    text: 'Seguire il recap',
+                    done: true
+                },
+                {
+                    text: 'Tagliare l\'erba',
+                    done: false
+                },
+                {
+                    text: 'Aggiustare il computer',
+                    done: false
+                },
+            ],
+        },
+        methods: {
+            
+        }
+    }
+)
